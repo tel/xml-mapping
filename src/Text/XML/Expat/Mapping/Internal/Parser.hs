bdc000ddfd60a76c3d2ff40ae1dc687861672b64
Joseph Abrahamson <me@jspha.com>
2013-10-30 14:50:58 -0400

    Attributes -> Simple Types

--- src/Text/XML/Expat/Mapping/Internal/Parser.hs
+++ src/Text/XML/Expat/Mapping/Internal/Parser.hs
@@ -1,13 +1,16 @@
 {-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TupleSections              #-}
 {-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE TypeSynonymInstances       #-}
 
 module Text.XML.Expat.Mapping.Internal.Parser where
 
+import           Control.Applicative
 import           Control.Error                    hiding (err)
 import           Control.Lens
 import           Control.Monad.Error
@@ -19,17 +22,36 @@ import           Data.Attoparsec.Number
 import           Data.ByteString                  (ByteString)
 import           Data.Hashable
 import qualified Data.HashMap.Strict              as Map
+import qualified Data.List.NonEmpty               as NEL
 import           Data.Monoid
 import           Data.Text                        (Text)
 import qualified Data.Text                        as T
 import           Data.Text.Encoding               (decodeUtf8', encodeUtf8)
-import           GHC.Generics
+import           GHC.Generics                     (Generic)
 import           Text.XML.Expat.Tree
 
+-- | This is a very simple initial parse of the @hexpat@ tree. We
+-- don't gather 'QName's and 'NName's because that requires extra tree
+-- traversals and our parser will be collecting that information
+-- anyway. Tags are collected as 'Text' because I assume that they can
+-- be split around @':'@, but the main text is left as 'ByteString' to
+-- allow for bizarre things in the CDATA.
+type Tag = Node Text ByteString
+
+-- | Namespaces on tags. 'Free' implies no namespace (thus \"free\" to
+-- take whatever definition the current context might
+-- give). 'Namespace' allows injection of a particular fully qualified
+-- namespace, not an abbreviation tag as those are never meaningfully
+-- canonical.
 data Namespace = Free | Namespace Text
                deriving ( Show, Eq, Ord, Generic )
 instance Hashable Namespace
 
+-- | Nothing at all special right now--just collects errors. This
+-- would eventually be very useful if it could collect more
+-- information about the error context (in the context of @>>@, in the
+-- context of @<|>@) and peek ahead to find more errors. See
+-- Swierstra, of course.
 newtype ParseError = ParseError [String]
                    deriving ( Eq, Show, Ord, Monoid )
 
@@ -37,94 +59,196 @@ instance Error ParseError where
   noMsg    = ParseError ["Error{noMsg}"]
   strMsg s = ParseError [s]
 
+-- | A type synonym over @ErrorT e Identity@ so that we get a free
+-- 'Alternative' instance without having to create yet another failure
+-- monad.
+--
+-- TODO: replace this guy with a better error applicative.
 type Err e a = ErrorT e Identity a
 
-fmapE :: Functor m => (e -> e') -> ErrorT e m a -> ErrorT e' m a
+-- | 'fmap' over an error type in 'Err'.
+fmapE :: (e -> e') -> Err e a -> Err e' a
 fmapE f (ErrorT mit) = ErrorT $ fmap go mit where
   go (Left e)  = Left (f e)
   go (Right a) = Right a
 
+-- | Eliminator for 'Err'
 err :: (e -> c) -> (a -> c) -> Err e a -> c
 err f s (ErrorT (Identity it)) = case it of
   Left e  -> f e
   Right a -> s a
 
+-- | The 'LevelState' is the parse-constant contextual state at this
+-- \"level\" of the tree. It includes the current location, attributes
+-- (metadata) in scope, and a set of currently in-scope namespaces.
 data LevelState =
-  LS { _path  :: [(Namespace, Text)]
-     , _atmap :: Map.HashMap Namespace (Map.HashMap ByteString ByteString)
-     , _ctx   :: Map.HashMap Text Text
+  LS { _path       :: NEL.NonEmpty (Namespace, Text)
+     , _atmap      :: Map.HashMap Namespace (Map.HashMap Text ByteString)
+     , _namespaces :: Map.HashMap Text Text
      }
 makeLenses ''LevelState
 
+-- | The core parser type. Currently this is implemented as a stack of
+-- 'Monad's, but should be CPS transformed eventually. This could also
+-- be turned into a transformer itself, but it'd be much nicer to
+-- avoid that if possible.
 newtype Parser a = P {
   unP :: ReaderT LevelState (
-     ErrorT ParseError (State [NNode ByteString])
+     ErrorT ParseError (State [Tag])
      ) a
-  }
+  } deriving ( Functor, Applicative, Alternative )
 
-class FromXMLAttribute a where
-  fromXMLAttribute :: ByteString -> Err String a
+-- | *Internal.* Execute a 'Parser' in all its gory detail.
+runP :: Parser a -> LevelState -> [Tag] -> (Either ParseError a, [Tag])
+runP (P inner) level st = runState (runErrorT (runReaderT inner level)) st
 
-instance FromXMLAttribute ByteString where
-  fromXMLAttribute = return
+-- | *Internal.* Execute a 'Parser' in the context of another
+-- 'Parser'. This might eventually be reasonable to export publically,
+-- but right now it's just useful for 'xsOptional' and node descent.
+forkP :: Parser a -> Parser (Either ParseError a, [Tag])
+forkP parser = P $ ask >>= \level -> get >>= \st -> return (runP parser level st)
 
-instance FromXMLAttribute Text where
-  fromXMLAttribute =
+-- | XML Simple Types, i.e. types which can be deserialized from plain
+-- XML text.
+class FromSimpleXMLType a where
+  fromSimpleXMLType :: ByteString -> Err String a
+
+instance FromSimpleXMLType ByteString where
+  fromSimpleXMLType = return
+
+instance FromSimpleXMLType Text where
+  fromSimpleXMLType =
     ErrorT . return . fmapL (const "Could not decode UTF-8") . decodeUtf8'
 
+instance FromSimpleXMLType String where
+  fromSimpleXMLType = fmap T.unpack . fromSimpleXMLType
+
 -- | @xs:boolean@-style booleans
-instance FromXMLAttribute Bool where
-  fromXMLAttribute t = case t of
+instance FromSimpleXMLType Bool where
+  fromSimpleXMLType t = case t of
     "true"  -> return True
     "1"     -> return True
     "false" -> return False
     "0"     -> return False
     _       -> fail "not a boolean"
 
-instance FromXMLAttribute Number where
-  fromXMLAttribute bs = case A8.eitherResult (A8.parse A8.number bs `A8.feed` mempty) of
+instance FromSimpleXMLType Number where
+  fromSimpleXMLType bs = case A8.eitherResult (A8.parse A8.number bs `A8.feed` mempty) of
     Left e  -> fail e
     Right a -> return a
 
-instance FromXMLAttribute Integer where
-  fromXMLAttribute bs = fromXMLAttribute bs >>= \mayI ->
+instance FromSimpleXMLType Integer where
+  fromSimpleXMLType bs = fromSimpleXMLType bs >>= \mayI ->
     case mayI of
       I i -> return i
       D _ -> fail "expecting integer, saw decimal"
 
-instance FromXMLAttribute Double where
-  fromXMLAttribute bs = fromXMLAttribute bs >>= \mayD ->
+instance FromSimpleXMLType Double where
+  fromSimpleXMLType bs = fromSimpleXMLType bs >>= \mayD ->
     case mayD of
       I i -> return (fromIntegral i)
       D d -> return d
 
-instance FromXMLAttribute a => FromXMLAttribute (Maybe a) where
-  fromXMLAttribute = return . err (const Nothing) Just . fromXMLAttribute
-
 -- | Space separated list---assumes a UTF-8 encoding
-instance FromXMLAttribute a => FromXMLAttribute [Maybe a] where
-  fromXMLAttribute = mapM (fromXMLAttribute . encodeUtf8) . T.words <=< fromXMLAttribute
+instance FromSimpleXMLType a => FromSimpleXMLType [a] where
+  fromSimpleXMLType = mapM (fromSimpleXMLType . encodeUtf8) . T.words <=< fromSimpleXMLType
 
-attr :: FromXMLAttribute a => Namespace -> ByteString -> Parser a
+-- | Convert a parser into one that may fail.
+xsOptional :: Alternative f => Parser a -> Parser (f a)
+xsOptional parser = P $ do
+  (res, leftover) <- unP $ forkP parser
+  case res of
+    Left  _ -> return empty
+    Right a -> put leftover >> return (pure a)
+
+-- | Specializes 'xsOptional'.
+xsMaybe :: Parser a -> Parser (Maybe a)
+xsMaybe = xsOptional
+
+-- | Parse the value of an XML attribute out of the current parsing
+-- context.
+attr :: FromSimpleXMLType a => Namespace -> Text -> Parser a
 attr ns n = P $ do
   mayBs <- preview (atmap . ix ns . ix n)
   case mayBs of
     Nothing -> fail $ "No attribute " ++ show (ns, n)
-    Just bs -> err fail return $ fromXMLAttribute bs
+    Just bs -> err fail return $ fromSimpleXMLType bs
 
-attr' :: FromXMLAttribute a => ByteString -> Parser a
+attr' :: FromSimpleXMLType a => Text -> Parser a
 attr' = attr Free
 
--- runParser :: Parser a -> Namespace -> ByteString -> UNode ByteString -> Either ParseError a
--- runParser _      _  _ Text{} = fail "Expecting element, saw text"
--- runParser (P go) ns n
+-- | Pull a name apart into its prefix and its body. Technically this
+-- should ensure that any 'Namespace' is an @NCName@, but right now it
+-- just pull off any chunk before the first colon.
+--
+-- TODO: Make this smarter.
+--
+-- @
+-- >>> prefix "foo"
+-- (Free, "foo")
+-- >>> prefix "foo:bar"
+-- (Namespace "foo", "bar")
+-- @
+--
+prefix :: Text -> (Namespace, Text)
+prefix t = case T.split (==':') t of
+  []     -> (Free       , T.empty)
+  [n]    -> (Free       , n)
+  (n:ns) -> (Namespace n, T.concat ns)
+
+-- | Check to see whether a particular name matches the current
+-- context.
+thisElement :: Namespace -> Text -> Parser Bool
+thisElement ns name = P $ (== (ns, name)) <$> view (path . to NEL.head)
+
+-- | *Internal.* Concatenates a list of 'Tag's into a single
+-- 'ByteString'.
+--
+-- @
+-- >>> collectText []
+-- Just ""
+-- >>> collectText [Text "foo", Text "bar"]
+-- Just "foobar"
+-- >>> collectText [Text "foo", Text "bar", Element "foo" [] []]
+-- Nothing
+-- @
+collectText :: [Tag] -> Maybe ByteString
+collectText = foldr textIt (Just mempty) where
+  textIt :: Tag -> Maybe ByteString -> Maybe ByteString
+  textIt _          Nothing   = Nothing
+  textIt Element{}  _         = Nothing
+  textIt (Text bs') (Just bs) = Just (bs <> bs')
+
+-- | Parse XML body text as an XML simple type.
+xsSimple :: FromSimpleXMLType a => Parser a
+xsSimple = P $ do
+  ns <- get
+  case collectText ns of
+    Nothing -> fail "Expecting text, saw elements"
+    Just bs -> err fail finalize (fromSimpleXMLType bs)
+  where
+    finalize a = put [] >> return a
+
+-- | Descend into a particular node running a parser.
+--
+-- When we load into an element we capture the information in the
+-- start node into the 'LevelState' in several ways. First, we take
+-- the tag name itself and store it in the path. Second, we update the
+-- namespace context using any @xmlns@ XML attributes. Thirdly, we
+-- update the current attribute context to include all the attributes
+-- which remain.
+
+-- load :: Parser a -> UNode ByteString -> Parser a
+-- load _      _  _ Text{} = fail "Expecting element, saw text"
+-- load (P go) ns n
 --   (Element { eName = name
 --            , eAttributes = attrs
 --            , eChildren = chils
 --            }) = P $ do
---     ctx <- view _1
---     case ns of
---       Free ->
+--     -- ctx <- view ctx
+--     undefined
+--     -- case ns of
+--     --   Free ->
 
 -- data ParseError = ParseError { trying :: First String, errors :: [String] }
 --                 deriving ( Eq, Ord, Show )
